e77ab5e571a49c8969117db7861ac0cf030f95a0
William Souza <william.h.souza@hotmail.com>
2023-08-01 15:13:10 -0300

    v1.2

--- controladores/livro.js
+++ controladores/livro.js
@@ -1,4 +1,4 @@
-const { getTodosLivros } = require('../servicos/livro');
+const { getTodosLivros, getLivroPorId, insereLivro } = require('../servicos/livro');
 
 function getLivros(req, res) {
     try{
@@ -10,6 +10,32 @@ function getLivros(req, res) {
     }
 }
 
+function getLivro(req, res) {
+    try{
+        const id = req.params.id
+        const livro = getLivroPorId(id);
+        res.send(livro)
+    } catch(error) {
+        res.status(500)
+        res.send(error.message)
+    }
+}
+
+function postLivro(req, res){
+    try {
+        const livroNovo = req.body
+        insereLivro(livroNovo)
+        res.status(201)
+        res.send("Livro inserido com sucesso")
+    }catch(error){
+        res.status(500)
+        res.send(error.message)
+    }
+}
+
+
 module.exports = {
-    getLivros
+    getLivros,
+    getLivro,
+    postLivro
 }
--- rotas/livro.js
+++ rotas/livro.js
@@ -1,13 +1,13 @@
 const { Router } =  require("express");
-const {getLivros} = require("../controladores/livro");
+const {getLivros, getLivro, postLivro} = require("../controladores/livro");
 
 const router = Router();
 
-router.get('/', getLivros) 
+router.get('/', getLivros)
 
-router.post('/', (req, res) => {
-    res.send("Voce fez uma requisiçao do tipo Post!")
-});
+router.get('/:id', getLivro) 
+
+router.post('/', postLivro )
 
 router.patch('/', (req, res) => {
     res.send("Voce fez uma requisiçao do tipo Patch!")
