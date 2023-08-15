fb3c24668bdb98edf2c208f5d2f16c3fc4de623c
William Souza <william.h.souza@hotmail.com>
2023-08-15 17:13:43 -0300

    v1.4

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-const express = require('express');
-const app = express();
-app.use(express.json());
+const express = require("express")
+const rotaLivro = require("./rotas/livro")
+const cors = require("cors")
+
+const app = express()
+app.use(express.json())
+app.use(cors({origin: "*"}))
+
+app.use('/livros', rotaLivro)
+
 const port = 8000
-const rotalivro = require("./rotas/livro");
-
-
-app.use('/livros', rotalivro)
 
 app.listen(port, () => {
-    console.log(`http://localhost:${port}`)
+    console.log(`Escutando a porta ${port}`)
 })
--- controladores/livro.js
+++ controladores/livro.js
@@ -1,82 +1,85 @@
-const { getTodosLivros, getLivroPorId, insereLivro, modificaLivro, deletarLivroPorId, } = require('../servicos/livro');
+const { getTodosLivros, getLivroPorId, insereLivro, modificaLivro, deletaLivroPorId } = require("../servicos/livro")
 
 function getLivros(req, res) {
-    try{
-        const livros = getTodosLivros();
+    try {
+        const livros = getTodosLivros()
         res.send(livros)
-    } catch(error) {
+    } catch (error) {
         res.status(500)
         res.send(error.message)
-    }
+    } 
 }
 
 function getLivro(req, res) {
-    try{
+    try {
         const id = req.params.id
-        if (id && Number(id)){
-            const livro = getLivroPorId(id);
+
+        if(id && Number(id)) {
+            const livro = getLivroPorId(id)
             res.send(livro)
         } else {
             res.status(422)
-            res.send("id invalido!")
+            res.send("Id inválido")
         }
+    } catch (error) {
+        res.status(500)
+        res.send(error.message)
+    } 
+}
+
+function postLivro(req, res) {
+    try {
+        const livroNovo = req.body
+        if(req.body.nome) {
+            insereLivro(livroNovo)
+            res.status(201)
+            res.send("Livro inserido com sucesso")
+        } else {
+            res.status(422)
+            res.send("O campo nome é obrigatório")
+        }
+        
     } catch(error) {
         res.status(500)
         res.send(error.message)
     }
 }
 
-function postLivro(req, res){
+function patchLivro(req, res) {
     try {
-        const livroNovo = req.body
-        if(req.body.nome){
-            insereLivro(livroNovo)
-            res.status(201)
-            res.send("Livro inserido com sucesso")
-        }else{
-            res.status(422)
-            res.send("O campo nome é obrigatorio!")
-        }
-    }catch(error){
-        res.status(500)
-        res.send(error.message)
-    }
-}
-
-function patchLivro(req, res){
-    try{
         const id = req.params.id
-        if (id && Number(id)){
+
+        if(id && Number(id)) {
             const body = req.body
             modificaLivro(body, id)
-            res.send("Item modificado com sucesso!")
+            res.send("Item modificado com sucesso")
         } else {
             res.status(422)
-            res.send("id invalido!")
+            res.send("Id inválido")
         }
-    }catch(error){
+       
+    } catch(error) {
         res.status(500)
-        res.send(error.message)
+        res.send(error.message) 
     }
 }
 
 function deleteLivro(req, res) {
-    try{
+    try {
         const id = req.params.id
-        if (id && Number(id)){
-            deletarLivroPorId(id)
-            res.send("Livro deletado com sucesso!")
+
+        if(id && Number(id)) {
+            deletaLivroPorId(id)
+            res.send("livro deletado com sucesso")
         } else {
             res.status(422)
-            res.send("id invalido!")
+            res.send("ID inválido")
         }
-    }catch(error){
-       res.status(500)
-       res.send(error.message) 
-    }
-
+    } catch (error) {
+        res.status(500)
+        res.send(error.message)
+    } 
 }
-
 
 module.exports = {
     getLivros,
